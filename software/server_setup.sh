92ea9d969d9c08ca3caf398493d37affd8dfede4
dien-github <22520247@gm.uit.edu.vn>
2025-12-27 15:01:26 +0000

    refactor(setup): remove Flask setup and update FastAPI requirements

--- software/server_setup.sh
+++ software/server_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create a virtual environment and install dependencies
-python3 -m venv venv
+python3 -m venv .venv
 
 # Activate the virtual environment
-source venv/bin/activate
+source ./.venv/bin/activate
 
 # Install required Python packages
 pip install -r requirements.txt
